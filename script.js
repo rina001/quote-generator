05c0740d23a8e08379a37bebce54d21af1881d75
Rina Qiu <rinaqiu88@yahoo.com>
2020-10-10 23:25:40 -0700

    Add files

---
+++ script.js
@@ -0,0 +1,75 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+let apiQuotes = [];
+
+//Loading Spinner Shown
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+//Remove Loading Spinner
+function complete() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
+
+//Show new Quote
+function newQuote() {
+  loading();
+  //pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  // check if Author field is blank and replace it with 'unknown'
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  //check Quote length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  // Set Quote, Hide Loader
+  quoteText.textContent = quote.text;
+  complete();
+}
+
+//Get quotes from API
+async function getQuote() {
+  loading();
+  //We need to use a Proxy Url to make our API call in order to avoid
+  const proxyUrl = "https://afternoon-oasis-38920.herokuapp.com/";
+  const apiUrl = "https://type.fit/api/quotes";
+
+  try {
+    const response = await fetch(proxyUrl + apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    getQuote();
+  }
+}
+
+//Tweet Quote
+function tweetQuote() {
+  const twitterUrl =
+    "https://twitter.com/intent/tweet?text=${quoteText.innerText} - ${authorText.innerText}";
+  window.open(twitterUrl, "_blank");
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener("click", getQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
+//on load
+getQuote();
